171d83bb0711872ee42d1cb73ba24b5e4ac27cde
gustavo-ar <gustavo.almeidaraphael@gmail.com>
2026-05-23 17:36:39 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='roteiro-cancun-pagamentos-v4';
-const FILES=['./','./index.html','./manifest.json','./service-worker.js','./painel_vivo_viagem_playa_cancun_2026_pagamentos_v2.xlsx'];
+const CACHE='roteiro-cancun-v6-voos-custos';
+const FILES=['./','./index.html','./manifest.json','./service-worker.js','./painel_vivo_v6_voos_custos_atualizados.xlsx'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).catch(()=>null)));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
